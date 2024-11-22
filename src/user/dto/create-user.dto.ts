790765f5a286c2c60dec56a686de3dc816f2b76c
vlzcrz <velizcruzv@gmail.com>
2024-11-21 21:28:16 -0300

    feat: Minor fix Data restriction has now been added, some attributes of POST and PATCH user have now constraint of maximum input characters.

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,13 +1,17 @@
-import { IsBoolean, IsEmail, IsString } from 'class-validator';
+import { IsBoolean, IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @Length(0, 15)
   nombre: string;
   @IsString()
+  @Length(0, 100)
   apellido: string;
   @IsEmail()
+  @Length(0, 100)
   correo_electronico: string;
   @IsString()
+  @Length(0, 30)
   contrasena: string;
   @IsBoolean()
   esta_eliminado: boolean;
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,12 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class UpdateUserDto {
   @IsString()
+  @Length(0, 15)
   nombre: string;
   @IsString()
+  @Length(0, 100)
   apellido: string;
   @IsEmail()
+  @Length(0, 100)
   correo_electronico: string;
 }
